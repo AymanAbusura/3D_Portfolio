28cb6a199df9be8b5181189d252b84c95db7eeaa
Ayman Salameh <ayman@MacBook-Air-Ayman.local>
2024-07-30 18:14:17 +0300

    Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+// import { ComputersCanvas } from "./canvas";
 
 //addnew
 import { useTranslation } from 'react-i18next';
@@ -30,7 +30,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
