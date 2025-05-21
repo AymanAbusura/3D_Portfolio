2fbdeaf70646090242a8b14a749595b0a4fb0fa7
Ayman Abusura <aymansalameh1996@hotmail.com>
2025-05-21 17:12:45 +0300

    Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const ServiceCard = ({ index, title, icon }) => (
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
         <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
-          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+          <img src={icon} alt={title} className='object-contain' width={64} height={64} loading="lazy" decoding="async" />
           <h3 className='text-white text-[20px] font-bold text-center'>
             {title}
           </h3>
