a68a1dcff9abebbb5d54a7537cb16eae255bc63d
Ayman Abusura <aymansalameh1996@hotmail.com>
2025-05-21 15:55:47 +0300

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1600, }
-})
+  build: {
+    chunkSizeWarningLimit: 1600,
+    minify: 'esbuild', // or 'terser'
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-icons')) {
+              return 'vendor_icons';
+            }
+            if (id.includes('@react-three/drei')) {
+              return 'vendor_three_drei';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
